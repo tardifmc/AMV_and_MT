42c6660af28cf954c34d8ecc3010994f34c7d24a
tardifmc <moniquectardif@gmail.com>
2026-02-06 00:46:45 -0500

    Wedding party adjusted

---
+++ Thoughts_friends.R
@@ -0,0 +1,11 @@
+Prince:   
+  MT: My oldest friend. From the recesses at Sainte-Madeleine to discovering ourselves and each other in 2026. thank you for having stood by me throughout all of our chapters. Thank you for seeing me in a way that will always be humbling and grounded, and allowing me to grow alongside you on our journeys.
+  
+Gergana:
+  MT: Gee, the shenanigans we pulled...Thank you for listening to me and my stories throughout the years, and always believing in me and rooting for me. You always made me feel loved and at home, no matter what continent I was on. Thank you for always making time for a walk, to play pokémon, and/or drink bubble tea. 
+
+Kimberly: 
+  MT: Kimberly, I always knew that when we spent time together we're trying something new, and I'm going to laugh. From prom dresses to wedding gowns, thank you for sharing important life events with me and the little lunch moments in between. Thank you for taking the Finch East bus with me for year, even though there were faster ways home for you, and still driving in today to spend time together. Thank you for bringing joy and whimsy, so consistently, into my life.
+
+Leslie: 
+  MT: The leafography says it all. Shenanigans, Doctors, eating cake on a sidewalk after running around Oakland, lifting heavy metal, and drinking a year supply of slushies in 20 minutes. Thank you for making the darkest of times a little brighter. And thank you for loving cats N'at so unashamedly much. pat pat pat. And most of all, thank you for driving 15 hours out of your way and playing question games with a relative stranger who knows how to use a shovel. T  
